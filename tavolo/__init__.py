a28217e45eee3174062c3e14d21e84fdfbc67f77
elior <elior.cohen.p@gmail.com>
2022-07-11 13:01:36 +0300

    :construction_worker: Fix docs

--- tavolo/__init__.py
+++ tavolo/__init__.py
@@ -1,3 +1,16 @@
+from pathlib import Path
+
+import toml
+
 from . import embeddings
-from . import seq2vec
 from . import learning
+from . import seq2vec
+
+
+def get_version():
+    path = Path(__file__).resolve().parents[1] / 'pyproject.toml'
+    pyproject = toml.loads(open(str(path)).read())
+    return pyproject['tool']['poetry']['version']
+
+
+__version__ = get_version()
